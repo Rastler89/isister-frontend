3c682da3c0d0e7eb5f7ac0731c086bf8451b21e7
Rastler <rastler89@gmail.com>
2025-01-23 20:43:02 +0100

    [feat] pet: agregado perfil [feat] scroll-area: agregado [feat] table: agregado [feat] tabs: agregado [feat] textarea: agregado

---
+++ app/pets/[id]/page.tsx
@@ -0,0 +1,11 @@
+import PetHealthTracker from "../../../components/pet-health"
+
+const PetProfile = () => {
+    return (
+        <main className='flex-1'>
+            <PetHealthTracker />
+        </main>
+    )
+}
+
+export default PetProfile
