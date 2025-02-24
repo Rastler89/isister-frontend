01f06cbc13733084a091a0533fedf2d90b0dbebb
Rastler <rastler89@gmail.com>
2025-02-24 19:12:03 +0100

    -

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.rules({
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-console': 'off',
+  }),
 ];
 
 export default eslintConfig;
