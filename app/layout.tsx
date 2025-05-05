eb3017b9e54b50a5b803ed1df6eef874a37dee2f
Rastler <rastler89@gmail.com>
2025-05-05 18:03:05 +0200

    add matomo

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,19 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link rel="icon" href="/img/veterinario.ico" />
+        <script>
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//matomo.rastler.dev/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
       </head>
       <body className={inter.className}>
         <AuthProvider>
